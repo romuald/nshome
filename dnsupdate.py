81b1d89e956b7a41615a0c619e6177377832b09f
Romuald Brunet <romuald@chivil.com>
2014-04-27 14:24:56 +0200

    Add poll option

--- dnsupdate.py
+++ dnsupdate.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+This script is used to send dynamic updates to a DNS server
+to update a record to your IP address.
 
+Mainly useful for people with dynamic IP providers who
+have access to a customised DNS server
+"""
 import json
 import socket
 import urllib2
 
+from time import sleep, strftime
 from argparse import ArgumentParser, FileType
 from subprocess import Popen, PIPE
 
@@ -29,10 +36,13 @@ def parseargs():
                         help='TTL of record (default 5min)')
     parser.add_argument('-v', dest='verbose', action='store_true',
                         default=False, help='Be verbose')
+    parser.add_argument('-p', '--poll', type=int, default=0,
+                        help='Check for IP change every POLL minutes')
 
     return parser.parse_args()
 
 def get_ip():
+    """Retrieve ip from remote service"""
     # Force IPv4
     host4 = socket.gethostbyname(IPHOST)
     req = urllib2.Request('http://{}/'.format(host4),
@@ -44,10 +54,9 @@ def get_ip():
 
     return ip.strip()
 
-def main():
-    args = parseargs()
+def do_update(ip, args):
     data = {
-        'ip': get_ip(),
+        'ip': ip,
         'show': 'SHOW\n' if args.verbose else '',
     }
     data.update(vars(args))
@@ -55,13 +64,39 @@ def main():
 
     run = ['nsupdate', '-k', args.key.name]
     if args.verbose:
-        print "%% %s\n===\n%s\n===" % (' '.join(run), script)
+        print "%s %% %s\n===\n%s\n===" % (now(), ' '.join(run), script)
 
     proc = Popen(run, stdin=PIPE)
     proc.stdin.write(script)
     proc.stdin.flush()
     proc.stdin.close()
-    proc.wait()
+
+    return proc.wait() == 0
+
+def now():
+    # lazy
+    return strftime("%Y-%m-%d %H:%M:%S")
+
+def main():
+    args = parseargs()
+
+    ip_prev = None
+    while True:
+        ip = get_ip()
+
+        if ip != ip_prev:
+            print "%s IP changed to %s -> UPDATE" % (now(), ip)
+            if do_update(ip, args):
+                # If update failed, try again next time, don't wait for IP change
+                ip_prev = ip
+
+        if args.poll <= 0:
+            break
+
+        try:
+            sleep(60 * args.poll)
+        except KeyboardInterrupt:
+            break
 
 if __name__ == '__main__':
     main()
